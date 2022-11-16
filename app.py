b35bf11c3abf104152a9384c51eda1cc119b4210
Sanskruti Joshi <67256543+akinjisho@users.noreply.github.com>
2022-11-16 13:17:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 import streamlit as st
 import requests
 import os
-from tensorflow.keras.utils import load_img
+from tensorflow.keras.utils import load_image
 # Home UI 
 
 def main():
