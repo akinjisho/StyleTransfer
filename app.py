ed0e4cf92113e44abfe4b5ef4139e35ea2cbf7a4
Mansi Singh <82221645+MansiSingh1117@users.noreply.github.com>
2022-12-12 20:31:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ from matplotlib import gridspec
 import matplotlib.pylab as plt
 import tensorflow as tf
 import tensorflow_hub as hub
+import sys
+import imageio
 
 # Home UI 
 
@@ -29,27 +31,6 @@ def main():
     st.write(font_css, unsafe_allow_html=True)
     stleTransfer()
 
-def load_image(image_url, image_size=(256, 256), preserve_aspect_ratio=True): #256 because if set less image was not very good in the output
-  """Loads and preprocesses images."""
-  # Cache image file locally.
-  image_path = tf.keras.utils.get_file(os.path.basename(image_url)[-128:], image_url)
-  # Load and convert to float32 numpy array, add batch dimension, and normalize to range [0, 1].
-  img = tf.io.decode_image(
-      tf.io.read_file(image_path),
-      channels=3, dtype=tf.float32)[tf.newaxis, ...]
-  img = crop_center(img)
-  img = tf.image.resize(img, image_size, preserve_aspect_ratio=True)
-  return img
-
-def crop_center(image):
-  """Returns a cropped square image."""
-  shape = image.shape
-  new_shape = min(shape[1], shape[2])
-  offset_y = max(shape[1] - shape[2], 0) // 2
-  offset_x = max(shape[2] - shape[1], 0) // 2
-  image = tf.image.crop_to_bounding_box(
-      image, offset_y, offset_x, new_shape, new_shape)
-  return image
 
 def uploadImage(key, new_height=480):
 
@@ -64,51 +45,73 @@ def uploadImage(key, new_height=480):
     
     return cv2.cvtColor(preProcessImg(cv2.imread('sample.jpg'),new_height),cv2.COLOR_BGR2RGB)
 
+ # UI Options  
+    if tabs == 'Annotate Image':
+        annotateImg()
+    if tabs == 'Resize Image':
+        resizeImg()
+
 def preProcessImg(img, new_height=480):
     # Pre-processing image: resize image
     img = cv2.resize(img,(256,256))
     return img
 
-def loadImg():
-    #content_image_url = st.text_input('Content Image URL', 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Golden_Gate_Bridge_from_Battery_Spencer.jpg/640px-Golden_Gate_Bridge_from_Battery_Spencer.jpg')
-    #style_image_url = st.text_input('Style Image URL', 'https://upload.wikimedia.org/wikipedia/commons/0/0a/The_Great_Wave_off_Kanagawa.jpg')
-    output_image_size = 384
-    # The content image size can be arbitrary.
-    content_img_size = (output_image_size, output_image_size)
-    # The style prediction model was trained with image size 256 and it's the 
-    # recommended image size for the style image (though, other sizes work as 
-    # well but will lead to different results).
-    style_img_size = (256, 256)  # Recommended to keep it at 256.
-    
-    #content_image = load_image(content_image_url, content_img_size)
-    #style_image = load_image(style_image_url, style_img_size)
-    
-    content_image = uploadImage("content_image")
-    style_image = uploadImage("style_image")
-    #style_image = tf.nn.avg_pool(style_image, ksize=[3,3], strides=[1,1], padding='SAME')
 
-    return (content_image,style_image)
-
-def stleTransfer():
-
-    st.header("Image Cartoonification")
-
-    (content_image,style_image) = loadImg()
-
-    # Original Image
-    st.subheader("Input Images")
-    st.image(content_image)
-    st.image(style_image)
-    
-    st.subheader("Cartoonized Image")
-
-    hub_handle = 'https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2'
-    hub_module = hub.load(hub_handle)
 
     
-    output = hub_module(tf.constant(content_image), tf.constant(style_image))
+def cartoonization():
+    st.header("Annotate Image")
 
-    st.image(output)
+    img = uploadImage("annotation_img")
+    #Bilateral Blurring
+    img1b=cv2.bilateralFilter(img1g,3,75,75)
+    plt.imshow(img1b,cmap='gray')
+    plt.axis("off")
+    plt.title("AFTER BILATERAL BLURRING")
+    plt.show()
+
+#Creating edge mask
+    edges=cv2.adaptiveThreshold(img1b,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,3,3)
+    plt.imshow(edges,cmap='gray')
+    plt.axis("off")
+    plt.title("Edge Mask")
+    plt.show()
+
+#Eroding and Dilating
+    kernel=np.ones((3,3),np.uint8)
+    img1e=cv2.erode(img1b,kernel,iterations=5)
+    img1d=cv2.dilate(img1e,kernel,iterations=5)
+    plt.imshow(img1d,cmap='gray')
+    plt.axis("off")
+    plt.title("AFTER ERODING AND DILATING")
+    plt.show()
+
+#Clustering - (K-MEANS)
+    imgf=np.float32(img1).reshape(-1,3)
+    criteria=(cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER,20,1.0)
+    compactness,label,center=cv2.kmeans(imgf,5,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+    center=np.uint8(center)
+    final_img=center[label.flatten()]
+    final_img=final_img.reshape(img1.shape)
+
+    final=cv2.bitwise_and(final_img,final_img,mask=edges)
+    plt.imshow(final,cmap='gray')
+    plt.axis("off")
+    plt.savefig('output1', bbox_inches='tight')
+
+    plt.show()
+    
+def resizeImg():
+    st.header("Resize Image")
+
+    img = uploadImage("resize_img")
+
+    scaleFactor = st.slider("Times Image",1,10,1,1,key='resize')/5
+    scaledImg = cv2.resize(img, None, fx=scaleFactor, fy = scaleFactor, interpolation = cv2.INTER_LINEAR)
+
+    st.image(scaledImg)
+    pass
+
 
 
 if __name__ == "__main__":
